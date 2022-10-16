6acfe2c37b21ab42236c54b4dcb6e72be19772f5
HeilRulez <Rulez.Heil@gmail.com>
2022-10-16 18:13:01 +0600

    продолжение вёрстки

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,7 +1,17 @@
 export default function AboutProject() {
     return (
         <section className='about-project'>
-
+            <h2 className='about-project__header'>О проекте</h2>
+            <div className='about-project__content'>
+                <h4 className='about-project__text'>Дипломный проект включал 5 этапов</h4>
+                <p className='about-project__text-description'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                <h4 className='about-project__text'>На выполнение диплома ушло 5 недель</h4>
+                <p className='about-project__text-description'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                <p className='about-project__text-item'>1 неделя</p>
+                <p className='about-project__text-item_description'>Back-end</p>
+                <p className='about-project__text-item'>4 недели</p>
+                <p className='about-project__text-item_description'>Front-end</p>
+            </div>
         </section>
     );
 }
