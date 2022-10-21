d2880658805b9a627a0ca63764a5c9a10cb25807
HeilRulez <rulez.heil@gmail.com>
2022-10-22 02:29:52 +0600

    вёрстка

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -5,10 +5,12 @@ export default function Techs() {
     return (
         <section className='techs'>
             <HeaderTextOne text={'Технологии'} />
-            <h2 className='techs__header'>7 технологий</h2>
-            <p className='techs__description'>
-                На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
-            </p>
+            <div className='techs__content'>
+                <h2 className='techs__header'>7 технологий</h2>
+                <p className='techs__description'>
+                    На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+                </p>
+            </div>
         </section>
     );
 }
