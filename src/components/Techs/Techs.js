64de71191329a463fa0ddfd498db82eb536eef0c
HeilRulez <Rulez.Heil@gmail.com>
2022-10-23 00:11:54 +0600

    вёрстка

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -10,6 +10,9 @@ export default function Techs() {
                 <p className='techs__description'>
                     На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
                 </p>
+                <ul className='techs__list'>
+                    <li className='techs__item'></li>
+                </ul>
             </div>
         </section>
     );
